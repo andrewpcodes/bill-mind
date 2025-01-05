826fd0bbc1bbe43d9630951414ff84a9c91f4807
andrewpcodes <apeterson247@gmail.com>
2025-01-05 16:32:22 -0500

    Initial Commit

---
+++ bill-mind/bill_mindApp.swift
@@ -0,0 +1,17 @@
+//
+//  bill_mindApp.swift
+//  bill-mind
+//
+//  Created by Andrew Peterson on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct bill_mindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
